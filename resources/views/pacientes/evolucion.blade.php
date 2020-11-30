dbc87223da7b07a4bb418c4f569488e6e26aab48
DelleDonneMati <matiasdeledone@hotmail.com.ar>
2020-11-30 17:08:08 -0300

    Se cambio como hacer la evolucion y es responsive

--- resources/views/pacientes/evolucion.blade.php
+++ resources/views/pacientes/evolucion.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
   <div class="row">
-    <div class="col-sm">
-    </div>
-    <div class="colum">
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <div style="background-color:#F0B8B8; margin: 70px 0px 10px; padding: 60px;">
         <h1>Datos del Paciente</h1>
         <br>
@@ -23,12 +22,15 @@
         </a>
         </div> 
     </div>
-        <div class="column" style="background-color:#96D1CD; margin: 70px 0px 10px; padding: 60px;">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="background-color:#96D1CD; margin: 70px 0px 10px; padding: 60px;">
             <h2>Evolucion</h2>
-            <div class="form-group" style="margin: 70px 0px 10px; padding: 60px;">
-                <input type="string" placeholder="Describa la evolucion del paciente" style=" padding: 80px;" class="form-control" id="evolucion" name="evolucion" required>
+            <div class="form-group">
+                <p>Describa la evolucion del paciente:</p>
+                <!-- <textarea name="textarea" href="{{ asset('css/textarea.css') }}"> </textarea> -->
+                <textarea name="textarea" id="evolucion" name="evolucion" rows="10" cols="50" required style="width:100%" ></textarea>
+                <!-- <input type="string" class="form-control" id="evolucion" name="evolucion" placeholder="Describa la evolucion del paciente" style=" height: 200px; margin: 70px 0px 60px; padding-left: 10px;" required> -->
             </div>
-            <button type="submit" class="btn btn-primary">Registrar</button>
+          <button type="submit" class="btn btn-primary">Registrar</button>
         </div>
         
     <div class="col-sm">
