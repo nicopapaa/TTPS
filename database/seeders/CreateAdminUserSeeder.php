99fd40dae35fc216296df353288b51d3e82ad4df
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-12-11 13:14:16 -0300

    Procfile

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -15,47 +15,6 @@ class CreateAdminUserSeeder extends Seeder
         */
         public function run()
         {
-        $user1 = User::create([
-        'name' => 'Sebastian',
-        'email' => 'sebastianesg@gmail.com',
-        'apellido' => 'Sanchez',
-        'dni' => 12345678,
-        'telefono' => 12345678,
-        'password' => bcrypt('123456')
-        ]);
-        $userAdmin = User::create([
-            'name' => 'administrador',
-            'email' => 'admin@gmail.com',
-            'apellido' => 'admin',
-            'dni' => 12345678,
-            'telefono' => 12345678,
-            'password' => bcrypt('123456')
-        ]);
-        $user2 = User::create([
-        'name' => 'Nicolas',
-        'email' => 'nicopapaa@gmail.com',
-        'apellido' => 'Paparazzo',
-        'dni' => 31785,
-        'telefono' => 31785,
-        'password' => bcrypt('123456')
-        ]);
-        $user3 = User::create([
-            'name' => 'Matias',
-            'email' => 'mati@gmail.com',
-            'apellido' => 'DelleDonne',
-            'dni' => 31785,
-            'telefono' => 31785,
-            'password' => bcrypt('123456')
-        ]);
-        $user4 = User::create([
-            'name' => 'Gonzalo',
-            'email' => 'gonza@gmail.com',
-            'apellido' => 'Bilbao',
-            'dni' => 31785,
-            'telefono' => 31785,
-            'password' => bcrypt('123456')
-        ]);
-        
         #Jefes de sistemas
         $jefe1 = User::create([
             'name' => 'Miguel',
@@ -127,18 +86,18 @@ class CreateAdminUserSeeder extends Seeder
         #admin
         $admin = User::create([
             'name' => 'Claudio',
-            'email' => 'medico3@gmail.com',
+            'email' => 'admin@gmail.com',
             'apellido' => 'Tapia',
             'dni' => 31785,
             'telefono' => 31785,
             'password' => bcrypt('123456')
         ]);
-        
+
         #creo el rol jefe de sistema
         $roleJefe = Role::create(['name' => 'Jefe de sistema']);
         $rolAdmin = Role::create(['name' => 'Administrador']);
-        $roleMedico=Role::create(['name' => 'Médico']);
-        
+        $roleMedico = Role::create(['name' => 'Médico']);
+
         #asigno role de jefes
         $jefe1 = assignRole([$roleJefe->id]);
         $jefe2 = assignRole([$roleJefe->id]);
@@ -146,21 +105,9 @@ class CreateAdminUserSeeder extends Seeder
         $jefe4 = assignRole([$roleJefe->id]);
         $jefe5 = assignRole([$roleJefe->id]);
 
-
-
-        
+        #asigno rol de medicos
         $medico1->assignRole([$roleMedico->id]);
         $medico2->assignRole([$roleMedico->id]);
         $medico3->assignRole([$roleMedico->id]);
-        $role=Role::create(['name' => 'Empleado']);
-        $user3->assignRole([$role->id]);
-        $role=Role::create(['name' => 'Organizacion']);
-        $user2->assignRole([$role->id]);
-        $user4->assignRole([$role->id]);
-        $role = Role::create(['name' => 'Admin']);
-        $permissions = Permission::pluck('id','id')->all();
-        $role->syncPermissions($permissions);
-        $user1->assignRole([$role->id]);
-        $userAdmin->assignRole([$role->id]);
     }
 }
