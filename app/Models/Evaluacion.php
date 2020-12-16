a1cde2a834be91fe99b588509e6f4b4d5c62a5a4
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-12-16 19:03:46 -0300

    test

---
+++ app/Models/Evaluacion.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Models;
+use Carbon\Carbon;
+
+class Evaluacion
+{
+    public function validar($param){
+        if($param == 1){
+            return true;
+        }
+        else
+            {
+                return false;
+            }
+    }
+
+    public function validar_mecanica($param){
+        if(($param == 2) or ($param == 3)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function validar_frecuencia($param){
+        if($param > 30){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function validar_fecha($end_date){
+        $now = Carbon::now()->toDateString();
+        $formatted_dt1=Carbon::parse($now);
+        $formatted_dt2=Carbon::parse($end_date);
+        $date_diff=$formatted_dt1->diffInDays($formatted_dt2);
+        if($date_diff == 10){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function validar_oxigeno($parm){
+        if($param < 92){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function validar_porcentaje($actual,$anterior){
+        $total = $actual - $anterior;
+        if($total > 3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
--- resources/views/alertasHistorial/index.blade.php
+++ resources/views/alertasHistorial/index.blade.php
@@ -10,10 +10,7 @@
     @foreach ( $alerta as $alertas)
         <div class="card"style="margin:50px;">
             <div class="card-body">
-            <a href="/leer/{{$alertas->id}}"><button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button></a>
-            <h5 class="card-title">Paciente - {{ $alertas->id }}</h5>
+            <h5 class="card-title">Paciente - {{ $alertas->id_paciente }}</h5>
             <p class="card-text">{{ $alertas->comentario }}</p>
             <p class="card-text">{{ $alertas->fecha }}</p>
             </div>
