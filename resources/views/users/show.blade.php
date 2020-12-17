8f929c43eae1649ca803010ad50cd6a9bc6f6c84
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-12-16 23:04:11 -0300

    foto

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,7 +21,7 @@
                 @endforeach </h6>
               </div>
               <div class="col-sm">
-              <img src="{{ asset('img/user.PNG') }}" style="width:250px; height:240px; ">
+              <img style="width:250px; height:300px; " src="https://us.123rf.com/450wm/alexutemov/alexutemov1604/alexutemov160400060/54585015-muchacho-m%C3%A9dico-personaje-de-dibujos-animados-y-de-juego-el-doctor-del-ni%C3%B1o-doctor-con-ni%C3%B1o-peque%C3%B1o-persona-est.jpg?ver=6">
               </div>
             </div>
           </div>
